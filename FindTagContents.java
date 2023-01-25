8badcd37eaa34e058c068a2729b7c932c30c52f0
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-25 12:13:26 -0600

    established first indexOf

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class FindTagContents {
     public static void main(String[] args) {
         
+        System.out.println("Enter HTML text");
+        Scanner scanner = new Scanner(System.in);
+        String html = scanner.nextLine();
+        
+        System.out.println("Enter tag type");
+        String tag = scanner.nextLine();
+
+        int textPos = html.indexOf("<" + tag + ">");
+
+        // int closingTagStart = html.indexOf(tag, ">");
+        // html.indexOf
     }
 }
